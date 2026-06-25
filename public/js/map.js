cb2eda95c4e05e3fad517bcdbe0ab388f8c5e7f9
Anuj kumar <anujkumarsingh3458@gmail.com>
2026-06-25 16:03:21 +0530

    map fixed

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,7 @@
 
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: [118.6923,34.0381], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
 
